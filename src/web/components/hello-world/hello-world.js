6a2dda083bc57f32d9937590cce5a8f8fd12e1af
nerdgrass <arboerstler@gmail.com>
2015-11-29 12:08:48 -0600

    adds helloWorld component

---
+++ src/web/components/hello-world/hello-world.js
@@ -0,0 +1,12 @@
+// Dependencies
+import React, { Component } from 'react'
+
+// Component
+export Chart = props => {
+  return (
+    <div className="hello-world">
+      <h2>Hello, world!</h2>
+      <p>Also, {props.name}</p>
+    </div>
+  )
+}
